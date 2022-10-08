1f3ea6184c3b805723810508651f7d277b348d59
sapta_finanku <saptahadiwijaya7@gmail.com>
2022-10-08 15:05:34 +0700

    tes2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Ok
+          Learn React Ok banget
         </a>
       </header>
     </div>
